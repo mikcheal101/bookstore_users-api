a22a69769780b47f267a6203bb4bbca823e76ab2
hirekaan.hemen <hirekaan.hemen@konga.com>
2021-07-02 00:24:41 +0100

    updated the user model to format for json

--- domain/users/User.go
+++ domain/users/User.go
@@ -1,10 +1,10 @@
 package users
 
 type User struct {
-	Id        int64
-	FirstName string
-	LastName  string
-	Email     string
-	CreatedAt string
-	UpdatedAt string
+	Id        int64  `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
 }
