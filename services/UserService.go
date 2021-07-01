12ec1cc5314f1a1ca18d5c5a089b6dee67f01616
hirekaan.hemen <hirekaan.hemen@konga.com>
2021-07-02 00:24:50 +0100

    created the user service

--- services/UserService.go
+++ services/UserService.go
@@ -1,1 +1,12 @@
 package services
+
+import "github.com/mikcheal101/bookstore_users-api/domain/users"
+
+type UserService struct{}
+
+/**
+* Method to create a user
+ */
+func (UserService *UserService) CreateUser(user users.User) (*users.User, error) {
+	return &user, nil
+}
