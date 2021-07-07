56641521c541fcc0a95773b345a158617943e801
hirekaan.hemen <hirekaan.hemen@konga.com>
2021-07-07 21:52:51 +0100

    created GetUserById

--- services/UserService.go
+++ services/UserService.go
@@ -8,12 +8,33 @@ import (
 type UserService struct{}
 
 /**
-* Method to create a user
+ * Method to create a user
  */
 func (UserService *UserService) CreateUser(user users.User) (*users.User, *errors.RestError) {
 	if err := user.ValidateUser(); err != nil {
 		return nil, err
 	}
 
-	return nil, nil
+	if err := user.Save(); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+/**
+ * Method to get a user by Id
+ */
+func (UserService *UserService) GetUserById(id int64) (*users.User, *errors.RestError) {
+	if id <= 0 {
+		err := errors.NewBadRequestError{}
+		err.BadRequest("Invalid user id")
+		return nil, &err.RestError
+	}
+
+	user := users.User{Id: id}
+	if userErr := user.Get(); userErr != nil {
+		return nil, userErr
+	}
+	return &user, nil
 }
