e24a698d71d58b6f797d93328b79c84ccfc74bb2
hirekaan.hemen <hirekaan.hemen@konga.com>
2021-07-07 01:33:08 +0100

    created a user validate method

--- domain/users/User.go
+++ domain/users/User.go
@@ -1,5 +1,11 @@
 package users
 
+import (
+	"strings"
+
+	"github.com/mikcheal101/bookstore_users-api/utils/errors"
+)
+
 type User struct {
 	Id        int64  `json:"id"`
 	FirstName string `json:"first_name"`
@@ -7,4 +13,14 @@ type User struct {
 	Email     string `json:"email"`
 	CreatedAt string `json:"created_at"`
 	UpdatedAt string `json:"updated_at"`
+}
+
+func (user *User) ValidateUser() *errors.RestError {
+	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
+	if user.Email == "" {
+		error := errors.NewBadRequestError{}
+		error.NewBadRequest("invalid  email address")
+		return &error.RestError
+	}
+	return nil
 }
