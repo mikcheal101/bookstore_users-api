d802b5b0d23c018bbc886d6eac52db3cf2af2856
hirekaan.hemen <hirekaan.hemen@konga.com>
2021-07-07 01:48:08 +0100

    dao for user created

---
+++ domain/users/User.dao.go
@@ -0,0 +1,13 @@
+package users
+
+import "github.com/mikcheal101/bookstore_users-api/utils/errors"
+
+// Method to save the user to the database
+func (user *User) Save() *errors.RestError {
+	return nil
+}
+
+// Method to get the user from the database by Id
+func (user *User) Get() *errors.RestError {
+	return nil
+}
