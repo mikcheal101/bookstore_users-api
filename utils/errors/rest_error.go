8cf1595a09d2c2828a722beba6c269f98cabb2a9
hirekaan.hemen <hirekaan.hemen@konga.com>
2021-07-07 01:33:32 +0100

    created NewBadRequest Method

--- utils/errors/rest_error.go
+++ utils/errors/rest_error.go
@@ -1,5 +1,7 @@
 package errors
 
+import "net/http"
+
 /**
 * Struct to manage Rest Errors
  */
@@ -7,4 +9,15 @@ type RestError struct {
 	Error   string `json:"error"`
 	Message string `json:"message"`
 	Status  int    `json:"status"`
+}
+
+type NewBadRequestError struct {
+	RestError RestError
+}
+
+func (newBadRequestError *NewBadRequestError) NewBadRequest(message string) *RestError {
+	newBadRequestError.RestError.Error = "bad_request"
+	newBadRequestError.RestError.Message = message
+	newBadRequestError.RestError.Status = http.StatusBadRequest
+	return &newBadRequestError.RestError
 }
