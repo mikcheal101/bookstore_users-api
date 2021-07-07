4fcd0a8ef49123acdfd4869ea5035393fc694d4f
hirekaan.hemen <hirekaan.hemen@konga.com>
2021-07-07 01:55:27 +0100

    NotFoundRequest error created

--- utils/errors/rest_error.go
+++ utils/errors/rest_error.go
@@ -15,9 +15,18 @@ type NewBadRequestError struct {
 	RestError RestError
 }
 
+// Method to throw bad request error
 func (newBadRequestError *NewBadRequestError) NewBadRequest(message string) *RestError {
 	newBadRequestError.RestError.Error = "bad_request"
 	newBadRequestError.RestError.Message = message
 	newBadRequestError.RestError.Status = http.StatusBadRequest
+	return &newBadRequestError.RestError
+}
+
+// Method to throw not found error
+func (newBadRequestError *NewBadRequestError) NotFoundRequest(message string) *RestError {
+	newBadRequestError.RestError.Error = "not_found"
+	newBadRequestError.RestError.Message = message
+	newBadRequestError.RestError.Status = http.StatusNotFound
 	return &newBadRequestError.RestError
 }
