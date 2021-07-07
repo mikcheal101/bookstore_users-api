257c3c9063a3b68b8c55c6db93f0b78e4bb722fe
hirekaan.hemen <hirekaan.hemen@konga.com>
2021-07-07 01:33:20 +0100

    implemented user validation method

--- services/UserService.go
+++ services/UserService.go
@@ -1,12 +1,19 @@
 package services
 
-import "github.com/mikcheal101/bookstore_users-api/domain/users"
+import (
+	"github.com/mikcheal101/bookstore_users-api/domain/users"
+	"github.com/mikcheal101/bookstore_users-api/utils/errors"
+)
 
 type UserService struct{}
 
 /**
 * Method to create a user
  */
-func (UserService *UserService) CreateUser(user users.User) (*users.User, error) {
-	return &user, nil
+func (UserService *UserService) CreateUser(user users.User) (*users.User, *errors.RestError) {
+	if err := user.ValidateUser(); err != nil {
+		return nil, err
+	}
+
+	return nil, nil
 }
